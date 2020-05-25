9d4993eb20d18e3e6c197beda4112f4e92e41fe5
eileentran <etran@risd.edu>
2020-05-25 15:49:29 -0400

    added back to top button

--- style.js
+++ style.js
@@ -47,3 +47,24 @@ $(document).ready(function () {
             .css("left", (e.pageX + xOff) + "px");
     });
 });
+
+
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
